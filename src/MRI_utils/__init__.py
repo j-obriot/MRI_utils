09fe6e94f37cc21f53145cc4bb9edf10f55ade8a
Joseph Obriot <joseph.obriot@cea.fr>
2026-04-07 09:14:49 +0200

    root init

---
+++ src/MRI_utils/__init__.py
@@ -0,0 +1,17 @@
+from . import (
+        bids,
+        dataset,
+        fmri,
+        kspace,
+        nifti,
+        plot,
+        )
+
+__all__ = [
+    "bids",
+    "dataset",
+    "fmri",
+    "kspace",
+    "nifti",
+    "plot",
+]
