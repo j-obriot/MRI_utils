5b0461e8db23fd817974eff4293b02187699a4f8
Joseph Obriot <joseph.obriot@cea.fr>
2026-04-07 10:38:27 +0200

    lazy loader

--- src/MRI_utils/__init__.py
+++ src/MRI_utils/__init__.py
@@ -1,11 +1,4 @@
-from . import (
-        bids,
-        dataset,
-        fmri,
-        kspace,
-        nifti,
-        plot,
-        )
+import importlib
 
 __all__ = [
     "bids",
@@ -15,3 +8,12 @@ __all__ = [
     "nifti",
     "plot",
 ]
+
+# lazy loader
+def __getattr__(name):
+    try:
+        module = importlib.import_module(f".{name}", __name__)
+        globals()[name] = module
+        return module
+    except:
+        raise AttributeError(f"module {__name__} has no attribute {name}")
